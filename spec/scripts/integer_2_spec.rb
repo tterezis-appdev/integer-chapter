50c2c9585404356ae5bcf2ba218a2f8b0df30386
osha7 <ogroetz@gmail.com>
2023-01-25 15:30:22 -0600

    updating integer birth year to be more suggestive for students to calculate using current year

--- spec/scripts/integer_2_spec.rb
+++ spec/scripts/integer_2_spec.rb
@@ -17,7 +17,7 @@ describe "integer_odd.rb" do
 end
 
 describe "integer_birth_year.rb" do
-  it "should output 'Wow, you were born in 1940. You're old!' if the input is 80.", points: 1 do
+  it "should output 'Wow, you were born in 1943. You're old!' if the input is 80.", points: 1 do
 
     allow_any_instance_of(Object).to receive(:gets).and_return("80")
     year = Date.today.year - 80
@@ -31,7 +31,7 @@ describe "integer_birth_year.rb" do
 end
 
 describe "integer_birth_year.rb" do
-  it "should output 'Wow, you were born in 2000. You're old!' if the input is 20.", points: 1 do
+  it "should output 'Wow, you were born in 2003. You're old!' if the input is 20.", points: 1 do
     # Un-require integer_birth_year.rb
     integer_birth_year = $".select{|r| r.include? 'integer_birth_year.rb'}
     $".delete(integer_birth_year.first)

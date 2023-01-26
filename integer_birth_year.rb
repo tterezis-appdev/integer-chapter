75b37c900d00849000c15d80b299262df848fa75
osha7 <ogroetz@gmail.com>
2023-01-25 18:05:32 -0600

    update comment with year assumption

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -6,7 +6,7 @@
 # Input:
 #   80
 # 
-# Key output:
-#   "Wow, you were born in 1943. You're old!"
+# Key output (assuming the current year is 2023): 
+# "Wow, you were born in 1943. You're old!"
 
 p "How old are you?"

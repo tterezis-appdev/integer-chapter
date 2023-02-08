4e4f93ef774580d668ec3057683722c80e6557b5
Ben Purinton <ben.purinton@gmail.com>
2023-02-08 19:05:27 +0000

    additions to birth_year and math

--- birth_year.rb
+++ birth_year.rb
@@ -1,27 +1,20 @@
 # Ask for a person’s age (use gets), then use the current year
 # to calculate and display what year they were born in.
 #
-# Should work similarly (assuming the current year is 2023) to the following:
+# Should output (assuming the current year is 2023):
 #
 #   "How old are you?"
 #   80
 #   "Wow, you were born in 1943. You're old!"
 #
-# Use exactly the same copy ("Wow, you were born...") as above in the output.
-# but replace values using string addition or string interpolation.
-
+# Use exactly the same copy ("Wow, you were born...") as above,
+# but replace the year with the calculated year.
 
 p "How old are you?"
 
 
 # ~~~~~ Specs (make it do these things) ~~~~~
 #
-# math.rb should output '1' 
-#
-# odd.rb should output 'true' if the entered number is odd 
-#
-# odd.rb should output 'false' if the entered number is not odd 
-#
-# birth_year.rb should output 'Wow, you were born in 1940. You're old!' if the input is 80. 
+# birth_year.rb should output 'Wow, you were born in [<current year> - <input age>]. You're old!'
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ solutions.md
@@ -0,0 +1,9 @@
+# Solutions
+
+## birth_year.rb
+
+```ruby
+age = gets.chomp.to_i
+year = 2023 - age # exchange 2023 with whatever the current year is
+p "Wow, you were born in #{year}. You're old!"
+```
